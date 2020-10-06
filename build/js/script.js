172cc2727c95a63ecc3417e591c58e449d0d9e61
Sm3shNoy <eskey96@inbox.ru>
2020-10-06 10:25:56 +0400

    Добавляет основные надстройки для сайта

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,24 @@
-"use strict";
+'use strict';
 
-console.log("Script worked!");
+var toggleMenu = document.querySelector('.main-nav__toggle');
+var mainHeader = document.querySelector('.header');
+var mainNav = document.querySelector('.main-nav');
+
+mainNav.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-nav--nojs');
+
+toggleMenu.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+    mainHeader.classList.add('header--active');
+  } else {
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+    mainHeader.classList.remove('header--active');
+  }
+
+  mainHeader.classList.toggle('header--open');
+});
